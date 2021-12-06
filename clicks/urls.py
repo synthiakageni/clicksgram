943c6dd1e65cfe7a0daf960b76a47244ddcf9c21
synthiakageni <synthia.kageni@student.moringaschool.com>
2021-12-06 14:09:56 +0300

    update files and make changes

--- clicks/urls.py
+++ clicks/urls.py
@@ -13,3 +13,5 @@ urlpatterns = [
     path('profile/',views.profile, name='profile'),
     path('homepage/',views.homepage, name='homepage'),
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
