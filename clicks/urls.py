c9aeeb9176a4e32b1f6ae911afabf08ed2e6d10b
synthiakageni <synthia.kageni@student.moringaschool.com>
2021-12-07 09:13:39 +0300

    add url for search

--- clicks/urls.py
+++ clicks/urls.py
@@ -7,11 +7,12 @@ from . views import *
 # Application Views
 
 urlpatterns = [
-    path('', views.user_login, name='login'),
+    path(r'', views.homepage, name='homepage'),
     path('logout/', views.user_logout, name='logout'),
     path('signup/', views.user_signup, name='signup'),
     path('profile/',views.profile, name='profile'),
     path('homepage/',views.homepage, name='homepage'),
+    path('search_images/',views.search_images, name='search_images'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
